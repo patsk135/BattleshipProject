9c2f2df70ef0d0ef0aa45f268a21e3de82d83c80
Anapat Jiamwijitkul <pat_sk135@hotmail.com>
2019-10-19 19:45:23 +0700

    add: createBoard

---
+++ frontend/src/App.jsx
@@ -0,0 +1,94 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { socket } from './socket';
+
+import { LoginModal } from './components/LoginModal';
+import { OnlinePlayersTab } from './components/OnlinePlayersTab';
+import { InviteWindow } from './components/InviteWindow';
+import { MyStatusBox } from './components/MyStatusBox';
+import { LobbyChat } from './components/LobbyChat';
+import { AdminPage } from './components/AdminPage';
+import { Board } from './components/game/createBoard/Board';
+
+function App() {
+    const [user, setUser] = useState({});
+    const [users, setUsers] = useState({});
+    const [messages, setMessages] = useState([]);
+
+    const [showLogin, setShowLogin] = useState(true);
+    const closeShowLogin = () => setShowLogin(false);
+
+    const [showInviteWindow, setShowInviteWindow] = useState(false);
+    const closeInviteWindow = () => setShowInviteWindow(false);
+
+    useEffect(() => {
+        // Ping ///////////////////////////////////////////////
+        socket.on('pingToClient', payload => {
+            // console.log(payload);
+        });
+        setInterval(() => {
+            socket.emit('pingToServer', 'Ping!');
+        }, 20000);
+        //////////////////////////////////////////////////////
+
+        socket.on('returnUpdatedUser', payload => {
+            console.log('ReturnUpdatedUser: ');
+            console.log(payload);
+            setUser(payload.user);
+        });
+
+        socket.on('refreshOnlineUsers', payload => {
+            console.log('RefreshOnlineUsers: ');
+            // console.log(payload);
+            console.log(payload);
+            // console.log(users);
+            setUsers(payload.users);
+        });
+
+        socket.on('msgToClients', payload => {
+            setMessages(oldmsg => [...oldmsg, payload.message]);
+        });
+
+        socket.on('getInvitation', ({ event, clientId }) => {
+            console.log(`GetInvitation -> OppId: ${clientId}`);
+            setShowInviteWindow(true);
+            setUser(prevUser => {
+                return {
+                    ...prevUser,
+                    oppId: clientId,
+                };
+            });
+        });
+    }, []);
+
+    return (
+        <>
+            <Router>
+                <Switch>
+                    <Route path='/test'>
+                        <Board></Board>
+                    </Route>
+                    <Route path='/admin'>
+                        <AdminPage></AdminPage>
+                    </Route>
+                    <Route path='/'>
+                        <div className='App'>
+                            <header className='App-header'>
+                                <div>{<MyStatusBox user={user} />}</div>
+                                {showLogin && <LoginModal close={closeShowLogin} />}
+                                {<OnlinePlayersTab user={user} users={users}></OnlinePlayersTab>}
+                                {showInviteWindow && (
+                                    <InviteWindow close={closeInviteWindow} user={user} />
+                                )}
+                                <div>{<LobbyChat messages={messages} />}</div>
+                            </header>
+                        </div>
+                    </Route>
+                </Switch>
+            </Router>
+        </>
+    );
+}
+
+export default App;
---
+++ frontend/src/components/game/createBoard/Board.jsx
@@ -0,0 +1,155 @@
+import React, { useState } from 'react';
+
+export const Board = ({ user }) => {
+    const [squares, setSquares] = useState(new Array(64).fill(0));
+    const [ship, setShip] = useState([]);
+    const [numberOfShips, setNumberOfShips] = useState(0);
+    // const [count, setCount] = useState(0);
+
+    const ready = () => {
+        console.log('READY!');
+    };
+
+    const handleOnClick = index => {
+        if (numberOfShips === 4) {
+            console.log('You are already have 4 ships!');
+            return;
+        }
+        if (ship.length === 4) {
+            console.log('Ship size must be 4!');
+        } else if (squares[index] === 1) {
+            console.log('Already placed!');
+        } else {
+            // setCount(count + 1);
+            setShip(prev => [...prev, index]);
+            const newBoard = squares.map(x => x);
+            newBoard[index] = newBoard[index] === 0 ? 1 : 0;
+            // console.log(newBoard);
+            setSquares(newBoard);
+        }
+        // console.log(ship.length);
+    };
+
+    const placeAShip = () => {
+        if (ship.length !== 4) {
+            console.log('Ship size must be 4!');
+            const newBoard = squares.map(x => x);
+            for (let i = 0; i < 4; i++) {
+                newBoard[ship[i]] = 0;
+            }
+            setShip([]);
+            setSquares(newBoard);
+            return;
+        }
+        let check = false;
+        let check1 = false;
+        let check2 = false;
+        const checkConsecutive = ship
+            .sort((a, b) => a - b)
+            .map(v => {
+                return [v % 8, Math.floor(v / 8)];
+            });
+        // console.log(checkConsecutive);
+        for (let i = 1; i < 4; i++) {
+            if (checkConsecutive[0][0] === checkConsecutive[i][0]) {
+                check1 = true;
+            } else {
+                check1 = false;
+                break;
+            }
+        }
+        for (let i = 1; i < 4; i++) {
+            if (checkConsecutive[0][1] === checkConsecutive[i][1]) {
+                check2 = true;
+            } else {
+                check2 = false;
+                break;
+            }
+        }
+        // console.log('check1' + check1);
+        // console.log('check2' + check2);
+        if (check1) {
+            for (let i = 1; i < 4; i++) {
+                if (checkConsecutive[0][1] + i === checkConsecutive[i][1]) {
+                    check = true;
+                } else {
+                    check = false;
+                    break;
+                }
+            }
+        } else if (check2) {
+            for (let i = 1; i < 4; i++) {
+                if (checkConsecutive[0][0] + i === checkConsecutive[i][0]) {
+                    check = true;
+                } else {
+                    check = false;
+                    break;
+                }
+            }
+        }
+        // console.log(check);
+        if (check && numberOfShips < 4) {
+            setNumberOfShips(numberOfShips + 1);
+        } else {
+            if (numberOfShips === 4) {
+                console.log('You are already have 4 ships!');
+                console.log('Please Click Ready!');
+            } else {
+                console.log('Ship must be 4 consecutive blocks');
+                // console.log(ship);
+            }
+            const newBoard = squares.map(x => x);
+            for (let i = 0; i < 4; i++) {
+                newBoard[ship[i]] = 0;
+            }
+            setSquares(newBoard);
+        }
+        // setCount(0);
+        setShip([]);
+        // console.log(ship.length);
+    };
+
+    const renderSquare = () => {
+        return squares.map((value, index) => {
+            return (
+                <div
+                    key={index}
+                    onClick={e => handleOnClick(index)}
+                    style={{
+                        width: '12.5%',
+                        height: '12.5%',
+                        borderColor: 'black',
+                        borderStyle: 'solid',
+                    }}
+                >
+                    {value === 1 && (
+                        <div
+                            style={{
+                                backgroundColor: 'red',
+                                height: '100%',
+                                width: '100%',
+                            }}
+                        />
+                    )}
+                </div>
+            );
+        });
+    };
+
+    return (
+        <>
+            <div
+                style={{
+                    width: '500px',
+                    height: '500px',
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                }}
+            >
+                {renderSquare()}
+            </div>
+            {numberOfShips !== 4 && <button onClick={placeAShip}>Place this Ship.</button>}
+            {numberOfShips === 4 && <button onClick={ready}>Ready</button>}
+        </>
+    );
+};
