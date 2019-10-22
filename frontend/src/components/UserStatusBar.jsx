bce4eb706cce71753beeee5576d3911aa4bd3f11
wisatong <wisa.pow@gmail.com>
2019-10-23 03:35:06 +0700

    add profile image (react logo fornow)

--- frontend/src/components/UserStatusBar.jsx
+++ frontend/src/components/UserStatusBar.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import './css/UserStatusBar.css';
 
 export const UserStatusBar = ({ user }) => {
-    return (
-        <div className='myHelloBox'>
-            <li className='hello'>Hello, {user.name}</li>
-            <li className='MMR'>MMR: {user.mmr}</li>
-            <li className='status'>{user.status}</li>
-            <div className='buttonss'>
-                <li>
-                    <button className='primaryButton widthBt'>Ready</button>
-                </li>
-                <li>
-                    <button className='secondaryButton widthBt'>Logout</button>
-                </li>
-            </div>
+  return (
+    <div className='myHelloBox'>
+      <div className='profile-container'>
+        <div className='avatar'>
+          <img src='/logo192.png' alt=""/>
         </div>
-    );
+        <p className='hello'>Hello, {user.name}</p>
+        <p className='MMR'>MMR: {user.mmr}</p>
+        <p className='status'>{user.status}</p>
+      </div>
+      <div className='buttons-container'>
+        <button className='primaryButton'>Ready</button>
+        <button className='secondaryButton'>Logout</button>
+      </div>
+    </div>
+  );
 };
