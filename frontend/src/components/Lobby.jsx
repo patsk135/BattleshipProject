2e1f51fb92660a5fff04affa1c65adee8c688086
wisatong <wisa.pow@gmail.com>
2019-10-23 05:11:19 +0700

    extract lobby component

---
+++ frontend/src/components/Lobby.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { UserStatusBar } from './UserStatusBar';
+import { OnlinePlayersTab } from './OnlinePlayersTab';
+import { LobbyChat } from './LobbyChat';
+import './css/Lobby.css';
+
+const OnlineLobby = ({ user, users, messages }) => {
+  return (
+    <>
+      <div className='lobby-right-container'>
+        <UserStatusBar user={user} />
+        <LobbyChat messages={messages} />
+      </div>
+      <OnlinePlayersTab user={user} users={users} />
+    </>
+  );
+}
+
+export const Lobby = ({ user, users, messages }) => {
+  return (
+    <div className='lobby-container'>
+      {user.status === 'ONLINE'
+        ? <OnlineLobby user={user} users={users} messages={messages} />
+        : null
+  }
+    </div>
+  )
+}
