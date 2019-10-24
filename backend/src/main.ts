64adbbc7515482d9f592872bad821437933c8bcb
Anapat Jiamwijitkul <pat_sk135@hotmail.com>
2019-10-24 19:50:40 +0700

    fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,6 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(process.env.PORT || 4000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
