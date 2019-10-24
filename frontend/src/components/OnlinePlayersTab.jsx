fee61dad3ce1d15b3e88230a02cd9fb868a41146
wisatong <wisa.pow@gmail.com>
2019-10-25 04:50:59 +0700

    click to sort player

--- frontend/src/components/OnlinePlayersTab.jsx
+++ frontend/src/components/OnlinePlayersTab.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './css/OnlinePlayersTab.css';
 import './css/UserStatusBar.css';
 import { socket } from '../socket';
+
+const customSort = (arr, f) => Object.values(arr).sort(f);
+
+const sortMmr = (a, b) => a.mmr - b.mmr;
+const sortName = (a, b) => (a.name.attr > b.name.attr) - (a.name.attr < b.name.attr);
+const sortStatus = (a, b) => (a.status.attr > b.name.attr) - (a.name.attr < b.name.attr);
 
 export const OnlinePlayersTab = ({ user, users }) => {
     const onClick = oppId => {
@@ -10,17 +16,19 @@ export const OnlinePlayersTab = ({ user, users }) => {
         socket.emit('sendInvitation', oppId);
     };
 
+    const [sorted, setSorted] = useState(users);
+
     return (
         <div className='online-players-container'>
             <ul className='nav'>
                 <p>AVATAR</p>
-                <p>
+                <p onClick={() => setSorted(customSort(users, sortName))}>
                     ONLINE
                     <br />
                     PLAYERS
                 </p>
-                <p>STATUS</p>
-                <p>MMR</p>
+                <p onClick={() => setSorted(customSort(users, sortStatus))}>STATUS</p>
+                <p onClick={() => setSorted(customSort(users, sortMmr))}>MMR</p>
                 <p>
                     INVITE
                     <br />
@@ -28,9 +36,8 @@ export const OnlinePlayersTab = ({ user, users }) => {
                 </p>
             </ul>
             <div className='container'>
-                {Object.values(users)
+                {Object.values(sorted)
                     .filter(one => one.id !== user.id)
-                    .sort((a, b) => b.mmr - a.mmr)
                     .map(eachUser => (
                         <ul key={eachUser.id}>
                             <li>
