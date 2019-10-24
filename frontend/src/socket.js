aa511188eaa6d79e7b65815d80358091431bc482
totee1998 <totee1998@gmail.com>
2019-10-24 17:56:07 +0700

    fix

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,4 +1,4 @@
 import socketIOClient from 'socket.io-client';
 
-export const socket = socketIOClient('localhost:3000');
+export const socket = socketIOClient('localhost:4000');
 // export const socket = socketIOClient(process.env.REACT_APP_URL);
